55dee15d2de50380c536b53c183a305d2e7a62a4
BoringFool <1318593330@qq.com>
2017-12-04 18:04:31 +0800

    .

---
+++ src/com/zm/test/javat.java
@@ -0,0 +1,26 @@
+package com.zm.test;
+
+
+
+public class javat {
+
+	public static void main(String[] args) {
+		
+	}
+	int x;
+	int [] b= {2,3};
+	boolean is=true;
+	public int[] a() {
+		
+		for(int i=2;i<x;i++) {
+			for(int j=2;j<Math.sqrt(i);j++) {
+				if(i%j==0) {
+					is=false;
+					break;
+				}
+			}
+		}
+		return b;
+	}
+
+}
