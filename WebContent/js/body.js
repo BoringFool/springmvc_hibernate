f380fe4dee2c4fa195c6cf5c57187ef0dcda2ccd
BoringFool <1318593330@qq.com>
2018-01-03 07:34:41 +0800

    .
    简单成型，但是每次 切换都会从0开始，而且hover好像在鼠标移入和移出都各触发一次，还有在触发一次后，需要移除触发（就是非同一个div才触发，同一个div已经完成触发后再移动就不触发）

--- WebContent/js/body.js
+++ WebContent/js/body.js
@@ -1,5 +1,5 @@
 $().ready(function() {
-
+	/*数遍悬浮切换*/
 	$(".cir_d").hover(function() {
 		$(".cir_d").removeClass("add_class");
 		var a = $(this).attr("value");
@@ -7,17 +7,49 @@ $().ready(function() {
 		distance(a);
 	});
 
-	
+	 /*计算坐标*/
 	function distance(arg) {
 		var a = (arg - 1) * 43;
 		var b=-((arg-1)*364);
-		var c=b/50;
+		var c=arg-1;
 		$(".triangle").css("top", a);
-		var o = { speed: 1000, timeout: 2000 };
-		
-		setTimeout(function(){$(".sc_ul").css("left",b); }, o.timeout);
+		cc(b,c);
 	}
 	
+	
+	/*渐换*/
+	function cc(arg,arg_b){
+		var t=1;
+		
+		/*画面切换速度*/
+		var timer =setInterval(function(){
+			cir(t,arg,arg_b);
+			}, 150);
+		
+		/*需要被执行4次*/
+		function cir(arg_1,arg_2,arg_3){
+			var z=-(91*arg_1)*arg_3;/*1/4*/
+			$(".sc_ul").css("left",z);
+				t++;
+			/*判断是否停止*/
+			if(t==5){
+				t=1;
+				clearInterval(timer);
+			}
+		}		
+	}
+	
+	
+	
+	/**/
+	
+	$("body").click(function(){
+		var p=$(".sc_ul").css("left");
+		var y=p.substring(0,p.length-2);
+		if(-1>-2){
+			alert(y);
+		}
+	});
 	
 	
 });
