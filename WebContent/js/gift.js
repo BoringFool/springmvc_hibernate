451a37061903b8767b11fe52dac4bb6d61a8305a
BoringFool <1318593330@qq.com>
2018-01-21 07:32:37 +0800

    .
    短时间多次快速触发，会导致一直转，是正转或者反转，不知道什么原因导致终止判断语句失效。

--- WebContent/js/gift.js
+++ WebContent/js/gift.js
@@ -14,4 +14,44 @@ $().ready(function (){
 		$(this).find("h3").css({"color":"#333333","white-space":"nowrap"});
 		$(this).find(".view_data").css("display","block");
 	});
+	
+	$(".sec .title_l").hover(function(){
+			ratate_i();
+	},function(){
+			ratate_i_b();		
+	});
+	
+	
+	
+	
+	function ratate_i(){
+		var a=5;
+		var b=1;
+		timer=setInterval(function(){
+			a=a+5;
+			$(".sec .title_l .r_fl_fr i").css("transform","rotate("+a+"deg)");
+			b++;
+			if(b>35){
+				a=0;
+				clearInterval(timer);
+			}
+		},8);
+		
+	}
+	
+	function ratate_i_b(){
+		var a=175;
+		var t=1;
+		timer=setInterval(function(){
+			a=a-5;
+			$(".sec .title_l .r_fl_fr i").css("transform","rotate("+a+"deg)");
+			t++;
+			if(t>35){
+				clearInterval(timer);
+			}
+		},8);
+		
+	}
+	
+
 });
